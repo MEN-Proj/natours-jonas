35c86995bc165a0b6fa8063a46fe2edd1499918e
hasan.mehedi <hasan.mehedi@bjitgroup.com>
2022-02-20 02:38:54 +0600

    refactor more code

--- routes/tours/tour.routes.js
+++ routes/tours/tour.routes.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const {
   getAllTours,
-  checkBody,
   createTour,
   getTour,
   updateTour,
   deleteTour,
-  checkID,
 } = require('../../controllers/tours/tours.controller');
 
 const tourRouter = express.Router();
 
-tourRouter.param('id', checkID);
-
-tourRouter.route('/').get(getAllTours).post(checkBody, createTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
