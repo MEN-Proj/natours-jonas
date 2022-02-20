7b47eddb28bdcc488703ee414b9c1c3b9d985498
hasan.mehedi <hasan.mehedi@bjitgroup.com>
2022-02-20 23:18:46 +0600

    implement: common routes using middleware

--- routes/tours/tour.routes.js
+++ routes/tours/tour.routes.js
@@ -5,11 +5,14 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  aliasTopTours,
 } = require('../../controllers/tours/tours.controller');
 
 const tourRouter = express.Router();
 
 tourRouter.route('/').get(getAllTours).post(createTour);
+
+tourRouter.route('/top-5-tours').get(aliasTopTours, getAllTours);
 
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
