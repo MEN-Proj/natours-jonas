cf49d3c07c2f39c0bc96d9baca96ccb18bb9cd3a
hasan.mehedi <hasan.mehedi@bjitgroup.com>
2022-03-01 02:41:56 +0600

    implement: api-rate-limit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('express-async-errors');
+const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 const { tourRouter } = require('./routes/tours/tour.routes');
 const { userRouter } = require('./routes/users/user.routes');
@@ -10,10 +11,23 @@ const {
 
 const app = express();
 
-app.use(express.json());
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const apiLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message:
+    'Too many accounts created from this IP, please try again after an hour',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+// Apply the rate limiting middleware to API calls only
+app.use('/api', apiLimiter);
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
